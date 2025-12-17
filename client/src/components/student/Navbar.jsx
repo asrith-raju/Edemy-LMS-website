47b6f4aef78d2f3e2ae221753f23bbba97f55e3c
asrith-raju <asrithraju143@gmail.com>
2025-12-17 19:54:40 +0530

    client(feat): add Signin feature

--- client/src/components/student/Navbar.jsx
+++ client/src/components/student/Navbar.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import { assets } from '../../assets/assets'
 import { Link } from 'react-router-dom'
+import { useClerk,UserButton,useUser } from '@clerk/clerk-react'
 
 const Navbar = () => {
 
   const isCourseListPage = location.pathname.includes('/course-list')
+
+  const {openSignIn} = useClerk()
+  const {user} = useUser()
 
   return (
     <div className={`flex items-center justify-between px-4 sm:px-10 md:px-14 lg:px-36 border-b border-gray-500 py-4 ${isCourseListPage ? 'bg-white':'bg-cyan-100/70'} `} >
@@ -14,7 +18,9 @@ const Navbar = () => {
           <button>Become Educator</button> |
           <Link to='/my-enrollments'>My Enrollments</Link>
          </div>
-         <button className='bg-blue-600 text-white px-5 py-2 rounded-full '>Create Account</button>
+         {user ? <UserButton/> :
+          
+          <button onClick={()=>openSignIn()} className='bg-blue-600 text-white px-5 py-2 rounded-full '>Create Account</button>}
       </div>
 
      {/* {for Phone Screens} */}
