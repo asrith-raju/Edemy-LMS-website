a4a6cb627983f185a6a9957d98d1b2a90acf3e3b
asrith-raju <asrithraju143@gmail.com>
2025-12-20 18:35:15 +0530

    client(feat): add testimonials section headings

--- client/src/components/student/Testimonials.jsx
+++ client/src/components/student/Testimonials.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 const Testimonials = () => {
   return (
     <div>
-      Testimonials
+        <h2>Testimonials</h2>
+        <p>Hear from learners as they share their journeys of transformation,sucess ,and our <br/>platform has made a difference in their lives.</p>
     </div>
   )
 }
