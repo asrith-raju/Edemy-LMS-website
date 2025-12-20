b41f4927883ffe1b56360078a4ecbb3aa74b08e6
asrith-raju <asrithraju143@gmail.com>
2025-12-20 19:19:38 +0530

    client(feat): add footer content

--- client/src/components/student/Footer.jsx
+++ client/src/components/student/Footer.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import { assets } from '../../assets/assets'
 
 const Footer = () => {
   return (
-    <div>
-      <h1>Footer</h1>
-    </div>
+    <footer>
+      <div>
+          <div>
+            <img src={assets.logo_dark} alt="logo" />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam consequatur tempore at distinctio soluta delectus quae facilis illum, ab obcaecati.</p>
+          </div>
+          <div></div>
+          <div></div>
+      </div>
+      <p>Copyright 2025 @ GreatStack.All Right Reserved.</p>
+    </footer>
   )
 }
 
