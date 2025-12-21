e3fae805a8a805f8ce8ab0646d27c76c305a9ea3
asrith-raju <asrithraju143@gmail.com>
2025-12-21 17:15:46 +0530

    client(feat): add email and subscribe button in footer

--- client/src/components/student/Footer.jsx
+++ client/src/components/student/Footer.jsx
@@ -4,10 +4,10 @@ import { assets } from '../../assets/assets'
 const Footer = () => {
   return (
     <footer className='bg-gray-900 md:px-36 text-left w-full mt-10'>
-      <div className='flex flex-col md:flex-row items-start px-8 md:px-0 justify-center gap-10 md:gap-32 py-10 border border-white/30'>
-          <div className='flex flex-col md:items-center md:text-left text-sm text-white/80'>
+      <div className='flex flex-col md:flex-row items-start px-8 md:px-0 justify-center gap-10 md:gap-32 py-10 border-b border-white/30'>
+          <div className='flex flex-col md:items-start items-center w-full'>
             <img src={assets.logo_dark} alt="logo" />
-            <p className='mt-6 text-center md:text-left text-sm text-white/80'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam consequatur tempore at distinctio soluta delectus quae facilis illum, ab obcaecati.</p>
+            <p className='mt-6 text-center md:text-left text-sm text-white/80'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam porro modi quam sequi! Corrupti, natus..</p>
           </div>
           <div className='flex flex-col md:items-start items-center w-full'>
             <h2 className='font-semibold text-white mb-5'>Company</h2>
@@ -21,6 +21,11 @@ const Footer = () => {
           <div className='hidden md:flex flex-col items-start w-full'>
             <h2 className='font-semibold text-white mb-5'>Subscribe to our newsletter</h2>
             <p className='text-sm text-white/80'>The latest news, articles, and resources, sent to your inbox weekly.</p>
+            <div className='flex items-center gap-2 pt-2'>
+              <input type="email" placeholder='Enter your email' 
+              className='border border-gray-500/30 bg-gray-800 text-gray-500 placeholder-gray-500  outline-none w-64 h-9 rounded px-2 text-sm'/>
+              <button className='bg-blue-600 w-24 h-9 text-white rounded'>Subsccribe</button>
+            </div>
           </div>
       </div>
       <p className='py-4 text-center text-xs md:text-sm text-white/80'>Copyright 2025 @ GreatStack.All Right Reserved.</p>
