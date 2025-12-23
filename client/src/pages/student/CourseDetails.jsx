4fe99c7c60c71fea6e710172c3733b42b3ca9271
asrith-raju <asrithraju143@gmail.com>
2025-12-23 19:09:55 +0530

    client(feat): add toggle functionality to each course section

--- client/src/pages/student/CourseDetails.jsx
+++ client/src/pages/student/CourseDetails.jsx
@@ -12,6 +12,8 @@ const CourseDetails = () => {
 
   const [courseData,setCourseData] = useState(null)
 
+  const [openSections,setOpenSections] = useState({})
+
   const {allCourses,calculateRating,calculateChapterTime,calculateNoOfLectures,CalculateCourseDuration} = useContext(AppContext)
 
 
@@ -23,6 +25,14 @@ const CourseDetails = () => {
   useEffect(() => {
    fetchCourseData( )
   }, [])
+
+  const toggleSection = (index)=>{
+      setOpenSections((prev)=>(
+         {...prev,
+          [index]:!prev[index]
+         }
+      ))  
+  }
   
   return courseData ? (
     <>
@@ -54,7 +64,7 @@ const CourseDetails = () => {
                    <div className='pt-5'>
                     {courseData.courseContent.map((chapter,index)=>(
                       <div key={index} className='border border-gray-300 bg-white mb-2 rounded'>
-                        <div className='flex items-center justify-between px-4 py-3 cursor-pointer select-none'>
+                        <div className='flex items-center justify-between px-4 py-3 cursor-pointer select-none' onClick={()=>toggleSection(index)}>
                           <div className='flex items-center gap-2'>
                             <img src={assets.down_arrow_icon} alt="arrow_icon" />
                             <p className='font-medium md:text-base text-sm'>{chapter.chapterTitle}</p>
@@ -62,7 +72,7 @@ const CourseDetails = () => {
                           <p className='text-sm md:text-default'>{chapter.chapterContent.length} lectures - {calculateChapterTime(chapter)}</p>
                         </div>
 
-                        <div className='overflow-hidden transition-all duration-300 max-h-98'>
+                        <div className={`overflow-hidden transition-all duration-300 ${openSections[index]?'max-h-96':'max-h-0'}`}>
                           <ul className='list-disc md:pl-10 pl-4 pr-4 py-2 text-gray-600 border-t border-gray-300'>
                             {chapter.chapterContent.map((lecture,i)=>(
                               <li key={i} className='flex items-start gap-2 py-1'>
