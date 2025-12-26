e18eea73e6b101fef33d0bd18c882ff75963e3eb
asrith-raju <asrithraju143@gmail.com>
2025-12-26 22:37:22 +0530

    client(feat): add click functionality to button in table

--- client/src/pages/student/MyEnrollments.jsx
+++ client/src/pages/student/MyEnrollments.jsx
@@ -4,7 +4,7 @@ import { useState } from 'react'
 
 const MyEnrollments = () => {
 
-  const {enrolledCourses ,CalculateCourseDuration} = useContext(AppContext)
+  const {enrolledCourses ,CalculateCourseDuration,navigate} = useContext(AppContext)
 
   const [progressArray, setProgressArray] = useState([
     {lectureCompleted:2,totalLectures:4},
@@ -56,7 +56,7 @@ const MyEnrollments = () => {
                 {progressArray[index] && `${progressArray[index].lectureCompleted}/ ${progressArray[index].totalLectures}`}<span>Lectures</span>
                </td>
                <td className='px-4 py-3 max-sm:text-right'>
-                <button className='px-3 sm:px-5 py-1.5 sm:py-2 bg-blue-600 max-sm:text-xs text-white'>
+                <button onClick={()=>navigate('/player/'+course._id)} className='px-3 sm:px-5 py-1.5 sm:py-2 bg-blue-600 max-sm:text-xs text-white'>
                   {progressArray[index] && progressArray[index].lectureCompleted / progressArray[index].totalLectures === 1 ? 'Completed':'On Going' }
                   </button>
                </td>
