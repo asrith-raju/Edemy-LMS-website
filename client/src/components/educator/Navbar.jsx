22722e6d2c661d8fef9626194424e430de32824d
asrith-raju <asrithraju143@gmail.com>
2025-12-29 17:12:50 +0530

    client(feat): add logo in the Educator Navbar

--- client/src/components/educator/Navbar.jsx
+++ client/src/components/educator/Navbar.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import { assets, dummyEducatorData } from '../../assets/assets'
+import {UserButton,useUser} from '@clerk/clerk-react'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
+  const educatorData = dummyEducatorData
+  const {user} = useUser() 
   return (
     <div>
-      <h1>Educator Navbar</h1>
+      <Link to='/'>
+     <img src={assets.logo} alt="Logo" className='w-28 lg:w-32' />
+     </Link>
+     
     </div>
   )
 }
