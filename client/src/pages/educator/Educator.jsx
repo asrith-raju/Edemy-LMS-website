5040a73a0e0252ec86cd6931fa00520f4b8ac89a
asrith-raju <asrithraju143@gmail.com>
2025-12-29 18:15:01 +0530

    client(feat): Mount Footer in Educator page

--- client/src/pages/educator/Educator.jsx
+++ client/src/pages/educator/Educator.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Navbar from '../../components/educator/Navbar'
 import SideBar from '../../components/educator/SideBar'
+import Footer from '../../components/educator/Footer'
 
 const Educator = () => {
   return (
@@ -12,6 +13,7 @@ const Educator = () => {
         <div className='flex-1'>
         {<Outlet/>} 
         </div>
+        <Footer/>
         </div>
     </div>
   )
