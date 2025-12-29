b86fe8885cd04e74d6b03ce78146e5fb5ebe40e5
asrith-raju <asrithraju143@gmail.com>
2025-12-29 18:11:07 +0530

    client(feat): add content in the Educator Footer component

--- client/src/components/educator/Footer.jsx
+++ client/src/components/educator/Footer.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import { assets } from '../../assets/assets'
 
 const Footer = () => {
   return (
-    <div>
-      <h1>edu footer</h1>
-    </div>
+    <footer className='flex md:flex-row flex-col-reverse items-center justify-between text-left w-full px-8 border-t'>
+     <div>
+      <img src={assets.logo} alt=""  className='hidden md:block w-20'/>
+      <div className='hidden md:block h-7 w-px bg-gray-500/60'></div>
+      <p className='py-4 text-center text-xs md:text-sm text-gray-500'>Copyright 2025 @ GreatStack.All Right Reserved.</p>
+     </div>
+    </footer>
   )
 }
 
