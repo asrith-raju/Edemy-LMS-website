005f84250196d50a16c1d8e5cea752e7ecd5b70c
asrith-raju <asrithraju143@gmail.com>
2025-12-30 19:59:32 +0530

    client(feat): Create state variables for form

--- client/src/pages/educator/AddCourse.jsx
+++ client/src/pages/educator/AddCourse.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import uniqid from 'uniqid'
 import Quill from 'quill'
 
 const AddCourse = () => {
+  const quillRef = useRef(null)
+  const editorRef = useRef(null)
+
+  const [courseTitle, setCourseTitle] = useState('')
+  const [coursePrice, setCoursePrice] = useState(0)
+  const [discount, setDiscount] = useState(0)
+  const [image, setImage] = useState(null)
+  const [chapters, setChapters] = useState([])
   return (
     <div>
       <h1>Add course page</h1>
