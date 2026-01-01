04a22faf3a37efcd2403bd29fc0f5552d378221e
asrith-raju <asrithraju143@gmail.com>
2026-01-01 16:58:18 +0530

    client(feat): Create input element for CourseThumbnail in AddCourse page

--- client/src/pages/educator/AddCourse.jsx
+++ client/src/pages/educator/AddCourse.jsx
@@ -50,7 +50,14 @@ const AddCourse = () => {
                <input onChange={e=>setCoursePrice(e.target.value)} value={coursePrice} type="number" placeholder='0' className='outline-none md:py-2.5 w-28 py-2 px-3 rounded border border-gray-500' />
             </div>
         
-
+           <div className='flex md:flex-row flex-col items-center gap-3'>
+          <p>Course Thumbnail</p>
+          <label htmlFor="thumbnailImage" className='flex items-center gap-3'>
+            <img src={assets.file_upload_icon} alt="" className='p-3 bg-blue-500 rounded'/>
+            <input type="file"  id='thumbnailImage' onChange={e=>setImage(e.target.files[0])} accept = "image/* " hidden/>
+            <img className='max-h-10'  src={image ? URL.createObjectURL(image):''} alt="" />
+          </label>
+        </div>
       
         </div>
 
