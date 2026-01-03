650c043e969583876f45905259c5b6d0336406d2
asrith-raju <asrithraju143@gmail.com>
2026-01-03 22:32:33 +0530

    server(feat): Create getEnrolledStudentsData controller function

--- server/controllers/educatorController.js
+++ server/controllers/educatorController.js
@@ -103,3 +103,29 @@ export const getEducatorCourses = async (req,res)=>{
         res.json({success:false,message:error.message})
     }   
 }
+
+//Get enrolled students Data with Purchased data
+
+export const getEnrolledStudentsData = async (req,res)=>{
+    try {
+        const educator = req.auth().userId
+        const courses = await Course.find({educator})
+        const courseIds = courses.map(course=>course._id)
+      
+        const purchases = await Purchase.find({courseId:{$in:courseIds},status:'completed'}).populate('userId','name imageUrl').populate('courseId','courseTitle')
+
+        const enrolledStudents = purchases.map(purchase=>{
+            return {
+                student:purchase.userId,
+                courseTitle:purchase.courseId.courseTitle,
+                 purchaseDate : purchase.createdAt
+            }
+        })
+
+        res.json({success:true,enrolledStudents})
+    } catch (error) {
+        res.json({success:false,message:error.message})
+    
+    } 
+
+}
