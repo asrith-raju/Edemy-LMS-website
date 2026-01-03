da118d2d82482fc82849548a383c15c71bc6f857
asrith-raju <asrithraju143@gmail.com>
2026-01-03 18:07:42 +0530

    server(feat): Create chapterSchema

--- server/models/Course.js
+++ server/models/Course.js
@@ -1,6 +1,11 @@
 import mongoose from "mongoose";
 
-
+const chapterSchema =  new mongoose.Schema({
+    chapterId :{type:String,required:true},
+    chapterOrder:{type:Number,required:true},
+    chapterTitle:{type:String,required:true},
+    chapterContent:[lectureSchema]
+},{_id:false})
 
 const courseSchema = new mongoose.Schema({
     courseTitle : {type:String,required:true},
@@ -9,7 +14,7 @@ const courseSchema = new mongoose.Schema({
     coursePrice : {type:Number,required:true},
     isPublished : {type:Boolean,default:true},
     discount:{type:Number,required:true,min:0,max:100},
-    courseContent:[],
+    courseContent:[chapterSchema],
     courseRatings:[
         {userId:{type:String,rating:{type:Number,min:1,max:5}}}
     ],
